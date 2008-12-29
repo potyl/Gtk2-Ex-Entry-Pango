8336856a68b4900ae5d6c8f352ee72e06d76a97d
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2008-12-29 19:48:53 +0000

    Added button for debugging
    git-svn-id: http://svn.potyl.com/repos/cpan/Gtk2-Ex-Entry-Pango/trunk@1889 0eeaf444-2d20-0410-987c-9b9cd00b88ca

--- examples/simple.pl
+++ examples/simple.pl
@@ -16,18 +16,27 @@ sub main {
 	my $window = Gtk2::Window->new();
 	my $entry = Gtk2::Ex::Entry::Pango->new();
 	
-	my $vbox = new Gtk2::VBox(FALSE, 0);
-	$vbox->pack_start($entry, FALSE, FALSE, FALSE);
-	$vbox->set_focus_child($entry);
-	$window->add($vbox);
+	my $button = Gtk2::Button->new('Action');
+	
+	my $box = new Gtk2::HBox(FALSE, 0);
+	$box->pack_start($entry, TRUE, TRUE, 0);
+	$box->pack_start($button, FALSE, FALSE, 0);
+	$window->add($box);
 	
 	# Use pango markup
 	$entry->set_markup(
 		'<span style="italic">Pango markup</span> is <span underline="error" underline_color="red">NOT</span> hard'
 	);
+	$entry->set(
+		markup => '<b>smaller</b> text'
+	);
 	
 	$window->signal_connect(delete_event => sub { Gtk2->main_quit(); });
+	$button->signal_connect(clicked => sub {
+		printf "Markup is %s\n", $entry->get('markup');
+	});
 	
+	$window->set_default_size(850, -1);
 	$window->show_all();
 	Gtk2->main();
 
