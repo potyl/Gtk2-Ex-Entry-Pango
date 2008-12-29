9061d5bf8e0557895eac239cc46cbd861692d39d
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2008-12-29 18:22:19 +0000

    The API is complete but not working
    git-svn-id: http://svn.potyl.com/repos/cpan/Gtk2-Ex-Entry-Pango/trunk@1887 0eeaf444-2d20-0410-987c-9b9cd00b88ca

--- lib/Gtk2/Ex/Entry/Pango.pm
+++ lib/Gtk2/Ex/Entry/Pango.pm
@@ -1,10 +1,128 @@
 package Gtk2::Ex::Entry::Pango;
 
+=head1 NAME
+
+Gtk2::Ex::Entry::Pango - Gtk2 Entry that accepts Pango markup.
+
+=head1 SYNOPSIS
+
+	use Gtk2 qw(-init);
+	use Glib qw(TRUE FALSE);
+	use Gtk2::Ex::Entry::Pango;
+	
+	my $window = Gtk2::Window->new();
+	my $entry = Gtk2::Ex::Entry::Pango->new();
+	
+	# You can use any method defined in Gtk2::Entry or set_markup()
+	$entry->set_markup('<span style="italic">Pango markup</span> is easy');
+	
+	my $vbox = new Gtk2::VBox(FALSE, 0);
+	$vbox->pack_start($entry, FALSE, FALSE, FALSE);
+	$vbox->set_focus_child($entry);
+	$window->add($vbox);
+	
+	$window->signal_connect(delete_event => sub { Gtk2->main_quit(); });
+	
+	$window->show_all();
+	Gtk2->main();
+
+=head1 HIERARCHY
+
+C<Gtk2::Ex::Entry::Pango> is a subclass of L<Gtk2::Entry>.
+
+	Glib::Object
+	+----Glib::InitiallyUnowned
+	     +----Gtk2::Object
+	          +----Gtk2::Widget
+	               +----Gtk2::Entry
+	                    +----Gtk2::Ex::Entry::Pango
+
+
+=head1 DESCRIPTION
+
+C<Gtk2::Ex::Entry::Pango> is a L<Gtk2::Entry> that can accept Pango markup as
+input (for more information about Pango text markup language see 
+L<http://library.gnome.org/devel/pango/stable/PangoMarkupFormat.html>).
+
+=head1 INTERFACES
+
+	Glib::Object::_Unregistered::AtkImplementorIface
+	Gtk2::Buildable
+	Gtk2::CellEditable
+	Gtk2::Editable
+
+=head1 METHODS
+
+The following methods are added by this widget:
+
+=head2 new
+
+Creates a new instance.
+
+=cut
+
+
 use strict;
 use warnings;
 
+use Gtk2;
+
 our $VERSION = '0.01';
+
+use Glib::Object::Subclass 'Gtk2::Entry' =>
+;
+
+
+
+=head2 set_markup
+
+Sets the text of the entry .
+Parses str which is marked up with the Pango text markup language, setting the label's text and attribute list based on the parse results. If the str is external data, you may need to escape it with g_markup_escape_text() or g_markup_printf_escaped(): 
+
+Parameters:
+
+=over
+
+=item * $markup
+
+The text to add to the entry, the text is expecte to be using Pango markup. This
+means that even if no markup is used special characters like 'E<lt>','E<gt>' and
+&, ' and " need to be escaped. Keep in mind that Pango markup is a subset of
+XML.
+
+You might want to use the following code snipet for escaping the characters:
+
+	use Glib::Markup qw(escape_text);
+	$entry->set_markup(
+		sprintf "The <i>%s</i> <b>%s</b> fox <sup>jumps</sup> over the lazy dog",
+			map { escape_text($_) } ('quick', 'brown')
+	);
+
+=back	
+
+=cut
+
+sub set_markup {
+	my $self = shift;
+	my ($markup) = @_;
+	$self->set_text($markup);
+}
 
 
 # Return a true value
 1;
+
+
+=head1 AUTHORS
+
+Emmanuel Rodriguez E<lt>potyl@cpan.orgE<gt>.
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2008 by Emmanuel Rodriguez.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
