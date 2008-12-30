63bd06fac6f080dc0aab5a1252a9a7f455bcf340
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2008-12-30 09:48:37 +0000

    Can write markup
    git-svn-id: http://svn.potyl.com/repos/cpan/Gtk2-Ex-Entry-Pango/trunk@1897 0eeaf444-2d20-0410-987c-9b9cd00b88ca

--- examples/simple.pl
+++ examples/simple.pl
@@ -15,15 +15,28 @@ sub main {
 
 	my $window = Gtk2::Window->new();
 	my $entry = Gtk2::Ex::Entry::Pango->new();
+
+	my $markup = Gtk2::Ex::Entry::Pango->new();
 	
 	my $button_print = Gtk2::Button->new('Print');
 	my $button_markup = Gtk2::Button->new('Markup');
+	my $button_apply = Gtk2::Button->new('Aply');
 	
-	my $box = new Gtk2::HBox(FALSE, 0);
-	$box->pack_start($entry, TRUE, TRUE, 0);
-	$box->pack_start($button_print, FALSE, FALSE, 0);
-	$box->pack_start($button_markup, FALSE, FALSE, 0);
-	$window->add($box);
+	my $hbox_top = new Gtk2::HBox(FALSE, 0);
+	$hbox_top->pack_start($entry, TRUE, TRUE, 0);
+	$hbox_top->pack_start($button_print, FALSE, FALSE, 0);
+	$hbox_top->pack_start($button_markup, FALSE, FALSE, 0);
+	
+	my $hbox_bottom = new Gtk2::HBox(FALSE, 0);
+	$hbox_bottom->pack_start($markup, TRUE, TRUE, 0);
+	$hbox_bottom->pack_start($button_apply, FALSE, FALSE, 0);
+
+
+	my $vbox = new Gtk2::VBox(FALSE, 0);
+	$vbox->pack_start($hbox_top,    TRUE, TRUE, 0);
+	$vbox->pack_start($hbox_bottom, TRUE, TRUE, 0);
+
+	$window->add($vbox);
 	
 	# Use pango markup
 	$entry->set_markup(
@@ -42,6 +55,14 @@ sub main {
 		$entry->set(
 			markup => '<b>smaller</b> text'
 		);
+	});
+
+
+	$button_apply->signal_connect(clicked => sub {
+		$markup->signal_emit('activate');
+	});
+	$markup->signal_connect(activate => sub {
+		$entry->set_markup($markup->get_text);
 	});
 
 	
