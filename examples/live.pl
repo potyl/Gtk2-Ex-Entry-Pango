2135f0b8a64b7e2c0de5e900b806709c5ccb5859
Emmanuel Rodriguez <emmanuel.rodriguez@gmail.com>
2009-01-02 22:09:52 +0000

    Corrected a typo reported by Zentara
    git-svn-id: http://svn.potyl.com/repos/cpan/Gtk2-Ex-Entry-Pango/trunk@1949 0eeaf444-2d20-0410-987c-9b9cd00b88ca

--- examples/live.pl
+++ examples/live.pl
@@ -29,7 +29,7 @@ sub main {
 	my $markup = Gtk2::Entry->new();
 	my $entry = Gtk2::Ex::Entry::Pango->new();
 	
-	my $button_apply = Gtk2::Button->new('Aply');
+	my $button_apply = Gtk2::Button->new('Apply');
 	
 	my $hbox_bottom = new Gtk2::HBox(FALSE, 0);
 	$hbox_bottom->pack_start($markup, TRUE, TRUE, 0);
